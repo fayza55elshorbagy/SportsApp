c8bfff0e34bb2c33792ec377913c38fb5f45ba95
fayza55elshorbagy <47149903+fayza55elshorbagy@users.noreply.github.com>
2021-04-19 08:05:39 -0700

    Initial Commit

---
+++ SportsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SportsApp
+//
+//  Created by fayza on 4/19/21.
+//  Copyright © 2021 fayza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
